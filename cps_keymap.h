8b064b959dda4039fe70e4501cb29d18d74085d4
DeepKolos <k1206521365k@126.com>
2018-04-30 21:26:47 +0800

    完善win+n

--- cps_keymap.h
+++ cps_keymap.h
@@ -1,9 +1,12 @@
 #ifndef CPS_KEYMAP
 #define CPS_KEYMAP
 
-const int composite_key_len = 6;
+const int composite_key_len = 14;
 
 #include "cps_key.h"
+
+#define KC_0_TO_9_SET_LEN 10
+#define KC_0_TO_9_SET {KC_1,KC_2,KC_3,KC_4,KC_5,KC_6,KC_7,KC_8,KC_9,KC_0}
 
 composite_key_t composite_keymap[] = {
     // home + wasd start
@@ -29,62 +32,62 @@ composite_key_t composite_keymap[] = {
      // home + wasd end
      
      // shift + bs -> enter
-    //  {{KC_LSFT, KC_TAB},    2,
-    //  {KC_RIGHT},            1,
-    //  {},                    0,
-    //  DEFAULT_CPS_BASE},
+     {{KC_LSFT, KC_TAB},    2,
+     {KC_RIGHT},            1,
+     {},                    0,
+     DEFAULT_CPS_BASE},
      
      // home + r -> win + r
-    //  {{KC_HOME, KC_R},      2,
-    //  {KC_LGUI, KC_R},       2,
-    //  {},                    0,
-    //  DEFAULT_CPS_BASE},
+     {{KC_HOME, KC_R},      2,
+     {KC_LGUI, KC_R},       2,
+     {},                    0,
+     DEFAULT_CPS_BASE},
 
      // win + n start
      {{KC_HOME, KC_1},      2,
      {KC_LGUI, KC_1},       2,
-     {},                    0,
+     KC_0_TO_9_SET,         KC_0_TO_9_SET_LEN,
      DEFAULT_CPS_BASE},
 
      {{KC_HOME, KC_2},      2,
      {KC_LGUI, KC_2},       2,
-     {},                    0,
-     DEFAULT_CPS_BASE}
+     KC_0_TO_9_SET,         KC_0_TO_9_SET_LEN,
+     DEFAULT_CPS_BASE},
 
-    //  {{KC_HOME, KC_3},      2,
-    //  {KC_LGUI, KC_3},       2,
-    //  {},                    0,
-    //  DEFAULT_CPS_BASE},
+     {{KC_HOME, KC_3},      2,
+     {KC_LGUI, KC_3},       2,
+     KC_0_TO_9_SET,         KC_0_TO_9_SET_LEN,
+     DEFAULT_CPS_BASE},
 
-    //  {{KC_HOME, KC_4},      2,
-    //  {KC_LGUI, KC_4},       2,
-    //  {},                    0,
-    //  DEFAULT_CPS_BASE},
+     {{KC_HOME, KC_4},      2,
+     {KC_LGUI, KC_4},       2,
+     KC_0_TO_9_SET,         KC_0_TO_9_SET_LEN,
+     DEFAULT_CPS_BASE},
 
-    //  {{KC_HOME, KC_5},      2,
-    //  {KC_LGUI, KC_5},       2,
-    //  {},                    0,
-    //  DEFAULT_CPS_BASE},
+     {{KC_HOME, KC_5},      2,
+     {KC_LGUI, KC_5},       2,
+     KC_0_TO_9_SET,         KC_0_TO_9_SET_LEN,
+     DEFAULT_CPS_BASE},
 
-    //  {{KC_HOME, KC_6},      2,
-    //  {KC_LGUI, KC_6},       2,
-    //  {},                    0,
-    //  DEFAULT_CPS_BASE},
+     {{KC_HOME, KC_6},      2,
+     {KC_LGUI, KC_6},       2,
+     KC_0_TO_9_SET,         KC_0_TO_9_SET_LEN,
+     DEFAULT_CPS_BASE},
 
-    //  {{KC_HOME, KC_7},      2,
-    //  {KC_LGUI, KC_7},       2,
-    //  {},                    0,
-    //  DEFAULT_CPS_BASE},
+     {{KC_HOME, KC_7},      2,
+     {KC_LGUI, KC_7},       2,
+     KC_0_TO_9_SET,         KC_0_TO_9_SET_LEN,
+     DEFAULT_CPS_BASE},
 
-    //  {{KC_HOME, KC_8},      2,
-    //  {KC_LGUI, KC_8},       2,
-    //  {},                    0,
-    //  DEFAULT_CPS_BASE},
+     {{KC_HOME, KC_8},      2,
+     {KC_LGUI, KC_8},       2,
+     KC_0_TO_9_SET,         KC_0_TO_9_SET_LEN,
+     DEFAULT_CPS_BASE},
      
-    //  {{KC_HOME, KC_9},      2,
-    //  {KC_LGUI, KC_9},       2,
-    //  {},                    0,
-    //  DEFAULT_CPS_BASE}
+     {{KC_HOME, KC_9},      2,
+     {KC_LGUI, KC_9},       2,
+     KC_0_TO_9_SET,         KC_0_TO_9_SET_LEN,
+     DEFAULT_CPS_BASE}
      // win + n end
 };
 
